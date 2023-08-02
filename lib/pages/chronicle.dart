d29df7644edb5eddc2c9e0fbdc33ecd6cf9f809e
Jakub Slabý <jakubslaby09@gmail.com>
2023-08-02 11:17:38 +0200

    implement block comments and author links

--- lib/pages/chronicle.dart
+++ lib/pages/chronicle.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:dpc/dpc.dart';
 import 'package:dpc/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:path/path.dart' as p;
+
+final RegExp authorPattern = RegExp(r"\[(\d*?)\]\((.*?)\):?\n?([\S\s]*)");
 
 class ChroniclePage extends StatelessWidget {
   ChroniclePage(this.relativePath, BuildContext context, {super.key, this.markdown = false}) {
@@ -21,50 +24,87 @@ class ChroniclePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(relativePath),
-        actions: [
-          IconButton(
-            style: const ButtonStyle(
-              fixedSize: MaterialStatePropertyAll(Size.square(50))
-            ),
-            icon: const Icon(Icons.edit_note),
-            // TODO: insert a comment at cursor
-            onPressed: null,
-          )
-        ],
+        // actions: const [
+        //   IconButton(
+        //     style: ButtonStyle(
+        //       fixedSize: MaterialStatePropertyAll(Size.square(50))
+        //     ),
+        //     icon: Icon(Icons.edit_note),
+        //     onPressed: null,
+        //   )
+        // ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: SelectableText.rich(
             TextSpan(
-              children: formatComments(string),
+              children: formatText(string, context),
             )
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: const Icon(Icons.edit_outlined),
+        // TODO: insert a comment at cursor
+        onPressed: null,
       ),
     );
   }
 
-  // TODO: implement block comments
-  List<InlineSpan> formatComments(String string) {
+  List<InlineSpan> formatText(String string, BuildContext context) {
     List<InlineSpan> output = [];
 
     int startChar = 0;
-    bool insert = false;
+    bool inline = false;
+    bool block = false;
     for ((int, int) c in string.codeUnits.indexed) {
       final String char = String.fromCharCode(c.$2);
-      if("<>".contains(char) || c.$1 + 1 == string.length) {
-        final String substring = string.substring(startChar, c.$1);
+      if("<>".contains(char) && !block || block && char == "\n" && c.$1 + 1 < string.codeUnits.length && String.fromCharCode(string.codeUnits[c.$1 + 1]) != ">" || c.$1 == string.length - 1) {
+        String substring = string.substring(startChar, c.$1);
+        Person? author;
+        String? authorString;
+        if(inline || block) {
+          final match = authorPattern.firstMatch(substring);
+          if(match != null) {
+            substring = match.group(3) ?? substring;
+            author = App.pedigree?.people[int.parse(match.group(1)!)];
+            authorString = match.group(2);
+          }
+        }
 
-        if(insert) {
+        if(inline) {
           output.add(WidgetSpan(
             child: Card(
               margin: const EdgeInsets.all(0),
-              child: markdown ? MarkdownBody(data: substring) : Text(substring)
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if(author != null) Text(authorString!, style: Theme.of(context).textTheme.labelSmall),
+                  if(author != null) Icon(author.sex.icon),
+                  Flexible(child: markdown ? MarkdownBody(data: substring) : Text(substring)),
+                ],
+              ),
+            )
+          ));
+        } else if(block) {
+          substring = substring.replaceAll("\n>", "\n");
+          output.add(WidgetSpan(
+            child: Row(
+              children: [
+                if(author != null) Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+                  child: Column(
+                    children: [
+                      Icon(author.sex.icon),
+                      Text(authorString!, style: Theme.of(context).textTheme.labelSmall),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: markdown ? MarkdownBody(data: substring) : Text(substring),
+                ),
+              ],
             )
           ));
         } else {
@@ -78,13 +118,20 @@ class ChroniclePage extends StatelessWidget {
         }
 
         startChar = c.$1 + 1;
+        switch (char) {
+          case "<":
+            inline = true;
+          case ">":
+            if(inline) {
+              inline = false;
+            } else {
+              block = true;
+            }
+          case "\n":
+            block = false;
+        }
       }
 
-      if(char == "<") {
-        insert = true;
-      } else if (char == ">") {
-        insert = false;
-      }
     }
 
     return output;
